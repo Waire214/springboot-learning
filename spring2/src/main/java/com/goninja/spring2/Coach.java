daf1ec30ea16c56621cfc1666fe21c46ef135dcc
waire <waire.tega@gmail.com>
2024-01-17 15:32:31 +0100

    Dependency injection - interface - implementation - usage

---
+++ spring2/src/main/java/com/goninja/spring2/Coach.java
@@ -0,0 +1,6 @@
+package com.goninja.spring2;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ spring2/src/main/java/com/goninja/spring2/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.goninja.spring2;
+
+import org.springframework.stereotype.Component;
+
+@Component //marks the class as a spring bean and makes it available for dependency injection
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes!!!!!!!!";
+    }
+}
